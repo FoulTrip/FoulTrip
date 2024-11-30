3bcefad6d7908259044ef1c41d279aebc1cda18f
David Vasquez <83887793+FoulTrip@users.noreply.github.com>
2024-11-30 12:27:14 -0500

    fix: unused variable

--- components/landing/Experience/Tecnologies.tsx
+++ components/landing/Experience/Tecnologies.tsx
@@ -1,18 +1,19 @@
 type TechnologyCategory = {
-    type: string;
-    tecs: string[];
-  };
-  
-  type TechnologiesProps = {
-    [key: string]: TechnologyCategory;
-  };
-  
-function Tecnologies({tecnologies}: TechnologiesProps) {
-    return (
-        <>
-        
-        </>
-    )
+  type: string;
+  tecs: string[];
+};
+
+type TechnologiesProps = {
+  [key: string]: TechnologyCategory;
+};
+
+function Tecnologies({ tecnologies }: TechnologiesProps) {
+  console.log(tecnologies)
+  return (
+    <>
+
+    </>
+  )
 }
 
 export default Tecnologies
